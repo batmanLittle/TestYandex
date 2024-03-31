a3c4817863671dc75550e2e6574731e36b52860c
Ksenia Ivanova <fen10wa@yandex.ru>
2024-03-31 11:46:27 +0300

    сделала карусель1

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,30 @@
-// window.addEventListener("resize", function () {
-//   const screenWidth = window.innerWidth;
-//   if (screenWidth < 900) {
-//     const titleText = document.querySelector(".support-info__title-text");
+let slideIndex = 1;
+showSlides(slideIndex);
 
-//     titleText.innerHTML = "Чтобы поддержать Международный васюкинский турнир";
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
 
-//     var parentElement = document.querySelector(".support-info__title");
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
 
-//     var newElement = document.createElement("h2");
-//     newElement.innerHTML =
-//       'посетите лекцию на тему: <span class="support-info__span">"Плодотворная дебютная идея"</span>';
-
-//     parentElement.appendChild(newElement);
-//   }
-// });
-
-// посетите лекцию
-//               на тему:
-//               <span class="support-info__span"
-//                 >«Плодотворная дебютная идея»</span
-//               >
-
-// const handleResize = () => {
-//   const width = window.innerWidth;
-
-//   if (width >= 651) {
-//     if (width === 1366) {
-//       x = 1;
-//     } else {
-//       x = width / 1366;
-//     }
-//   }
-//   if (width <= 650) {
-//     if (width === 375) {
-//       x = 1;
-//     } else {
-//       x = width / 375;
-//     }
-//   }
-
-//   const page = document.querySelector(".page");
-//   page.style.zoom = `${x}`;
-
-//   if (width >= 1367) {
-//     page.style.zoom = `${1}`;
-//   }
-// };
-
-// handleResize();
-
-// window.addEventListener("resize", handleResize);
-
-//изменение текста в зависимости от шириный экрана support-info
-
-// const handleResizeText = () => {
-//   const element = document.querySelector(".one");
-//   const elementrr = document.querySelector(".one");
-//   if (width <= 650) {
-//     (element.textContent =
-//       "Чтобы поддержать Международный васюкинский турнир") &&
-//       (elementrr.textContent = "dddd");
-//   }
-// };
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mobile");
+  let dots = document.getElementsByClassName("transformation__carousel-dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].className += " active";
+}
