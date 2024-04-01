48c18f2c3deabfd0f9ebf80f225ad7bc3d209dbc
Tany <fen10wa@yandex.ru>
2024-04-01 11:00:34 +0300

    делаю вторую карусель в мобиле

--- pages/index.js
+++ pages/index.js
@@ -28,3 +28,26 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "flex";
   dots[slideIndex - 1].className += " active";
 }
+
+showCards(slideIndex);
+
+function plusCards(n) {
+  showCards((slideIndex += n));
+}
+
+function showCards(n) {
+  let i;
+  let slides = document.getElementsByClassName("participants__block");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "flex";
+}
