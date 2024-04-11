c94167fda28f2d76ec629cf09521be50b2c58f81
Ksenia Ivanova <fen10wa@yandex.ru>
2024-04-11 10:05:05 +0300

    dodelala footer

--- pages/index.js
+++ pages/index.js
@@ -29,25 +29,67 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
-// showCards(slideIndex);
+// 2ая карусель
 
-// function plusCards(n) {
-//   showCards((slideIndex += n));
-// }
+let cardIndex = 0;
 
-// function showCards(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("participants__block");
+function showSlide(n) {
+  const windowWidth = window.innerWidth;
+  const slides = document.querySelectorAll(".participants__card");
+  const spans = document.querySelectorAll(".participants__carousel-span");
 
-//   if (n > slides.length) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = slides.length;
-//   }
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
+  // Скрываем все карточки
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
 
-//   slides[slideIndex - 1].style.display = "flex";
-// }
+  // Проверка границ
+  if (windowWidth <= 850) {
+    if (n >= slides.length) {
+      cardIndex = 0;
+    } else if (n < 0) {
+      cardIndex = slides.length - 1;
+    }
+  } else {
+    if (n >= slides.length - 2) {
+      cardIndex = 0;
+    } else if (n < 0) {
+      cardIndex = slides.length - 3;
+    }
+  }
+
+  // Показываем нужную карточку
+  if (windowWidth <= 850) {
+    slides[cardIndex].style.display = "flex";
+  } else {
+    for (let i = cardIndex; i < cardIndex + 3; i++) {
+      slides[i].style.display = "flex";
+    }
+  }
+
+  // Обновляем значение span
+  for (let i = 0; i < spans.length; i++) {
+    if (windowWidth <= 850) {
+      spans[i].textContent = cardIndex + 1;
+    } else {
+      spans[i].textContent = cardIndex + 3;
+    }
+  }
+}
+
+function plusCards(n) {
+  showSlide((cardIndex += n));
+}
+
+function autoCarousel() {
+  plusCards(1);
+  setTimeout(autoCarousel, 4000);
+}
+
+// Вызов функции для автоматической смены слайдов
+
+window.addEventListener("resize", () => {
+  showSlide(cardIndex);
+});
+showSlide();
+autoCarousel();
